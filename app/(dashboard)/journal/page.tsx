36ae3f145d8cc84b617cf05aa0ca4995254691cb
Soohyun Lim <d2axasap@gmail.com>
2024-01-17 19:52:44 -0500

    Continue building the journal page

--- app/(dashboard)/journal/page.tsx
+++ app/(dashboard)/journal/page.tsx
@@ -1,5 +1,26 @@
-const Journal = () => {
+import { getUserByClerkID } from '@/utils/auth';
+import { prisma } from '@/utils/db';
+
+const getEntries = async () => {
+  const user = await getUserByClerkID();
+
+  const entries = await prisma.journalEntry.findMany({
+    where: {
+      userId: user.id,
+    },
+    orderBy: {
+      createdAt: 'desc', // newest at the top
+    },
+  });
+
+  return entries;
+};
+
+const JournalPage = async () => {
+  const entries = await getEntries();
+  console.log(entries);
+
   return <div>journal</div>;
 };
 
-export default Journal;
+export default JournalPage;
--- utils/db.ts
+++ utils/db.ts
@@ -10,7 +10,7 @@ const globalForPrisma = globalThis as unknown as {
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
-    log: ['query'],
+    // log: ['query'],
   });
 
 // only in development mode
